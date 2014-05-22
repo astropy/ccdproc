dbd8acf2024d120702630c3e6470661c034c6e44
Matthew Craig <mattwcraig@gmail.com>
2014-05-22 02:40:32 -0500

    Add the file containing  CaseInsensitiveOrderedDict
    Closes #78

---
+++ ccdproc/utils/collections.py
@@ -0,0 +1,35 @@
+"""
+Useful objects based on collections
+"""
+
+from __future__ import (absolute_import, print_function)
+
+from astropy.utils.compat.odict import OrderedDict
+
+
+class CaseInsensitiveOrderedDict(OrderedDict):
+    """
+    docstring for CaseInsensitiveOrderedDict
+    """
+
+    def __init__(self, *arg, **kwd):
+        super(CaseInsensitiveOrderedDict, self).__init__(*arg, **kwd)
+
+    def _transform_key(self, key):
+        return key.upper()
+
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveOrderedDict,
+              self).__setitem__(self._transform_key(key), value)
+
+    def __getitem__(self, key):
+        return super(CaseInsensitiveOrderedDict,
+                     self).__getitem__(self._transform_key(key))
+
+    def __delitem__(self, key):
+        return super(CaseInsensitiveOrderedDict,
+                     self).__delitem__(self._transform_key(key))
+
+    def __contains__(self, key):
+        return super(CaseInsensitiveOrderedDict,
+                     self).__contains__(self._transform_key(key))
