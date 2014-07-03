3b89bc241f41fc9e37b8ab0b88d55dec836b8327
Steven Crawford <crawfordsm@gmail.com>
2014-07-03 17:08:13 -0400

    added tests for rebinning

--- ccdproc/tests/test_ccdproc.py
+++ ccdproc/tests/test_ccdproc.py
@@ -16,7 +16,7 @@ from astropy.tests.helper import pytest
 
 from ..ccddata import CCDData
 from ..core import *
-from ..core import _rebin, _blkavg
+from ..core import _blkavg
 
 
 # test creating variance
@@ -426,23 +426,29 @@ def test_transform_image(ccd_data, mask_data, uncertainty):
 
 
 #test rebinning ndarray
-def test__rebin_ndarray(ccd_data):
+def test_rebin_ndarray(ccd_data):
     with pytest.raises(TypeError):
-        _rebin(1, (5, 5))
+        rebin(1, (5, 5))
 
 
 #test rebinning dimensions
 @pytest.mark.data_size(10)
-def test__rebin_dimensions(ccd_data):
+def test_rebin_dimensions(ccd_data):
     with pytest.raises(ValueError):
-        _rebin(ccd_data.data, (5,))
+        rebin(ccd_data.data, (5,))
+
+#test rebinning dimensions
+@pytest.mark.data_size(10)
+def test_rebin_ccddata_dimensions(ccd_data):
+    with pytest.raises(ValueError):
+        rebin(ccd_data, (5,))
 
 
 #test rebinning works
 @pytest.mark.data_size(10)
-def test__rebin_larger(ccd_data):
+def test_rebin_larger(ccd_data):
     a = ccd_data.data
-    b = _rebin(a, (20, 20))
+    b = rebin(a, (20, 20))
 
     assert b.shape == (20, 20)
     np.testing.assert_almost_equal(b.sum(), 4 * a.sum())
@@ -450,13 +456,34 @@ def test__rebin_larger(ccd_data):
 
 #test rebinning is invariant
 @pytest.mark.data_size(10)
-def test__rebin_smaller(ccd_data):
+def test_rebin_smaller(ccd_data):
     a = ccd_data.data
-    b = _rebin(a, (20, 20))
-    c = _rebin(b, (10, 10))
+    b = rebin(a, (20, 20))
+    c = rebin(b, (10, 10))
 
     assert c.shape == (10, 10)
     assert (c-a).sum() == 0
+
+#test rebinning with ccddata object
+@pytest.mark.parametrize('mask_data, uncertainty', [
+                         (False, False),
+                         (True, True)])
+@pytest.mark.data_size(10)
+def test_rebin_ccddata(ccd_data, mask_data, uncertainty):
+    if mask_data:
+        ccd_data.mask = np.zeros_like(ccd_data)
+    if uncertainty:
+        err = np.random.normal(size=ccd_data.shape)
+        ccd_data.uncertainty = StdDevUncertainty(err)
+
+    b = rebin(ccd_data, (20, 20))
+
+    assert b.shape == (20,20)
+    if mask_data:
+        assert b.mask.shape == (20,20)
+    if uncertainty:
+        assert b.uncertainty.array.shape == (20,20)
+
 
 
 #test blockaveraging ndarray
