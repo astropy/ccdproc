7f4aa6b4ada863b10d9d56c99577f676cdf5cdd2
me <me@localhost.localdomain>
2016-02-21 08:35:29 -0600

    Removed duplicate wcs property and setter in ccddata.py

--- ccdproc/ccddata.py
+++ ccdproc/ccddata.py
@@ -124,14 +124,6 @@ class CCDData(NDDataArray):
     @data.setter
     def data(self, value):
         self._data = value
-        
-    @property
-    def wcs(self):
-        return self._wcs
-    
-    @wcs.setter
-    def wcs(self, value):
-        self._wcs = value
 
     @property
     def wcs(self):
