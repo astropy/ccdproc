7c10ec03b5193c7085eda273a3ee93e9f4d1f9a7
Josh Walawender <jmwalawender@gmail.com>
2016-10-31 11:23:43 -1000

    Add ccdmask function

--- ccdproc/core.py
+++ ccdproc/core.py
@@ -27,7 +27,7 @@ __all__ = ['background_deviation_box', 'background_deviation_filter',
            'ccd_process', 'cosmicray_median', 'cosmicray_lacosmic',
            'create_deviation', 'flat_correct', 'gain_correct', 'rebin',
            'sigma_func', 'subtract_bias', 'subtract_dark', 'subtract_overscan',
-           'transform_image', 'trim_image', 'wcs_project', 'Keyword']
+           'transform_image', 'trim_image', 'wcs_project', 'Keyword', 'ccdmask']
 
 # The dictionary below is used to translate actual function names to names
 # that are FITS compliant, i.e. 8 characters or less.
@@ -1556,6 +1556,104 @@ def cosmicray_median(ccd, error_image=None, thresh=5, mbox=11, gbox=0,
 
     else:
         raise TypeError('ccd is not an numpy.ndarray or a CCDData object.')
+
+
+def ccdmask(ratio, ncmed=7, nlmed=7, ncsig=15, nlsig=15, lsigma=6, hsigma=6,
+            ngood=5):
+    '''
+    Uses method based on the IRAF ccdmask task to generate a mask based on the
+    given input.
+
+    Parameters
+    ----------
+    ratio : `~ccdproc.CCDData`
+        Data to used to form mask.  Typically this is a ratio of two flat field
+        images.
+
+    ncmed, nlmed: int, optional
+        The column and line size of a moving median rectangle used to estimate
+        the uncontaminated local signal. The column median size should be at
+        least 3 pixels to span single bad columns.
+
+    ncsig, nlsig: int, optional
+        The column and line size of regions used to estimate the uncontaminated
+        local sigma using a percentile. The size of the box should contain of
+        order 100 pixels or more.
+
+    lsigma, hsigma: float, optional
+        Positive sigma factors to use for selecting pixels below and above the
+        median level based on the local percentile sigma.
+
+    ngood: int, optional
+        Gaps of undetected pixels along the column direction of length less than
+        this amount are also flagged as bad pixels.
+
+
+    The Following documentation is copied directly from:
+    http://stsdas.stsci.edu/cgi-bin/gethelp.cgi?ccdmask
+
+    The input image is first subtracted by a moving box median. The median is
+    unaffected by bad pixels provided the median size is larger that twice the
+    size of a bad region. Thus, if 3 pixel wide bad columns are present then the
+    column median box size should be at least 7 pixels. The median box can be a
+    single pixel wide along one dimension if needed. This may be appropriate for
+    spectroscopic long slit data.
+
+    The median subtracted image is then divided into blocks of size nclsig by
+    nlsig. In each block the pixel values are sorted and the pixels nearest the
+    30.9 and 69.1 percentile points are found; this would be the one sigma
+    points in a Gaussian noise distribution. The difference between the two
+    count levels divided by two is then the local sigma estimate. This algorithm
+    is used to avoid contamination by the bad pixel values. The block size must
+    be at least 10 pixels in each dimension to provide sufficient pixels for a
+    good estimate of the percentile sigma. The sigma uncertainty estimate of
+    each pixel in the image is then the sigma from the nearest block.
+
+    The deviant pixels are found by comparing the median subtracted residual to
+    a specified sigma threshold factor times the local sigma above and below
+    zero (the lsigma and hsigma parameters). This is done for individual pixels
+    and then for column sums of pixels (excluding previously flagged bad pixels)
+    from two to the number of lines in the image. The sigma of the sums is
+    scaled by the square root of the number of pixels summed so that
+    statistically low or high column regions may be detected even though
+    individual pixels may not be statistically deviant. For the purpose of this
+    task one would normally select large sigma threshold factors such as six or
+    greater to detect only true bad pixels and not the extremes of the noise
+    distribution.
+
+    As a final step each column is examined to see if there are small segments
+    of unflagged pixels between bad pixels. If the length of a segment is less
+    than that given by the ngood parameter all the pixels in the segment are
+    also marked as bad.
+    '''
+    medsub = ratio.data - ndimage.filters.median_filter(ratio.data,
+                                                        size=(nlsig, ncsig))
+
+    nbl = int(np.floor(ratio.data.shape[0] / nlsig))
+    nbc = int(np.floor(ratio.data.shape[1] / ncsig))
+    mask = np.zeros(ratio.data.shape, dtype=np.bool)
+    for i in range(nbl):
+        for j in range(nbc):
+            block = medsub[i*nlsig:(i+1)*nlsig,j*ncsig:(j+1)*ncsig]
+            high = np.percentile(block.ravel(), 69.1)
+            low = np.percentile(block.ravel(), 30.9)
+            block_sigma = (high-low)/2.0
+            block_mask = ( (block > hsigma*block_sigma) |
+                           (block < -lsigma*block_sigma) )
+            mblock = np.ma.MaskedArray(block, mask=block_mask)
+            csum = np.ma.sum(mblock, axis=0)
+            csum_sigma = np.array([np.sqrt(ncsig-x)*block_sigma
+                                   for x in np.ma.sum(mblock.mask, axis=0)])
+            colmask = ( (csum > hsigma*csum_sigma) |
+                        (csum < -lsigma*csum_sigma) )
+            colmask = colmask.filled(True)
+            for c,masked in enumerate(colmask):
+                block_mask[:,c] = block_mask[:,c] | np.array([masked]*nlsig)
+                for r in range(ngood, len(block_mask[:,c])):
+                    if block_mask[r-ngood,c] and block_mask[r,c]:
+                        block_mask[r-ngood:r,c] = True
+            mask[i*nlsig:(i+1)*nlsig,j*ncsig:(j+1)*ncsig] = block_mask
+    return mask
 
 
 class Keyword(object):
