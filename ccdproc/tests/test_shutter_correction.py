de7ae871977868e6dabee18549e2251ca3f45a6a
Josh Walawender <jmwalawender@gmail.com>
2016-11-04 11:44:38 -1000

    Put basic skeleton of shutter_correction in

---
+++ ccdproc/tests/test_shutter_correction.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# This module implements the base CCDData class.
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+
+from ..core import *
+
+def test_shutter_correction_ifc(flatsIFC, biasesIFC):
+    pass
+
+def test_shutter_correction_ifc(flats_filenames, biases_filenames):
+    pass
+
