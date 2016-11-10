f747d9d7f74f1a793e2510bed9b25e64cfb24717
Josh Walawender <jmwalawender@gmail.com>
2016-11-10 09:26:24 -1000

    Split shutter correction algorithm out

--- ccdproc/shutter_correction.py
+++ ccdproc/shutter_correction.py
@@ -3,12 +3,90 @@ from __future__ import (print_function, division, absolute_import,
 
 import os
 
-from .core import *
+import numpy as np
+import astropy.units as u
+from astropy.nddata import StdDevUncertainty
+
+from .core import CCDData, subtract_bias
+from .ccddata import fits_ccddata_reader, fits_ccddata_writer
+from .combiner import combine
 from .image_collection import ImageFileCollection
+
+
+def shutter_correction_algorithm(combined_flats, exptimekey='EXPTIME',
+                       normalizer=lambda flat: flat.data.max(),
+                       output=None):
+    '''
+    Determine the shutter correction given a set of input bias subtracted flats.
+    
+    Parameters
+    ----------
+    combined_flats : `list` containing `~ccdproc.CCDData` objects
+        Set of bias subtracted flats of different exposure times taken under
+        constant illumination.
+    
+    exptimekey : `string`, defaults to 'EXPTIME'
+        The header keyword which contains the exposure time of the image in
+        seconds.
+
+    Notes
+    -----
+    This code follows the implementation described in Surma, P. (1993) 
+    "Shutter-free flatfielding for CCD detectors" Astronomy and Astrophysics
+    (ISSN 0004-6361), 278, 654–658.
+
+    Returns
+    -------
+    shutter_map : `~ccdproc.CCDData`
+        The shutter correction map in units of seconds.
+    '''
+    assert 'GAIN' in combined_flats[0].header.keys()
+    gain = combined_flats[0].header['GAIN']
+
+#     def nflat(flat, size=(10,10)):
+#         mflat = ndimage.filters.median_filter(flat.data, size=size)
+#         print(flat.data.max(), np.percentile(flat.data, 99), mflat.max())
+#         return mflat.max()
+
+
+    a = gain*np.sum([normalizer(f) for f in combined_flats])
+    b = gain*np.sum([normalizer(f)/float(f.header[exptimekey]) for f in combined_flats])
+    c = gain*np.sum([normalizer(f)/float(f.header[exptimekey])**2 for f in combined_flats])
+
+    Dijcomb = combine(combined_flats, method='average')
+    Dij = gain*Dijcomb.data*len(combined_flats)
+
+    Eijm = [CCDData(data=f.data/float(f.header[exptimekey]), unit=f.unit)
+            for f in combined_flats]
+    Eijcomb = combine(Eijm, method='average')
+    Eij = gain*Eijcomb.data*len(combined_flats)
+
+    alpha_ij = 1.0 / (a*c - b**2) * (c*Dij - b*Eij)
+    delta_alpha = np.sqrt( c / (a*c - b**2) )
+    beta_ij = 1.0 / (a*c - b**2) * (a*Eij - b*Dij)
+    delta_beta = np.sqrt( a / (a*c - b**2) )
+
+    SF = beta_ij / alpha_ij  # SF = -t_SH * (1-SH_ij) in the paper terminology
+    delta_SF = np.sqrt( (alpha_ij**-1 * delta_beta)**2 + 
+                        (beta_ij * alpha_ij**-2 * delta_alpha)**2 )
+    SNR = np.mean(abs(SF)/delta_SF)
+
+    shutter_map = CCDData(data=SF,
+                          uncertainty=StdDevUncertainty(delta_SF),
+                          unit=u.second,
+                          meta={'SNR': (SNR, 'Mean signal to noise per pixel')})
+
+    if output:
+        if os.path.exists(output):
+            os.remove(output)
+        fits_ccddata_writer(shutter_map, output)
+
+    return shutter_map
 
 
 def calculate_shutter_correction_map(files, biases=None,
                                      exptimekey='EXPTIME',
+                                     normalizer=lambda flat: flat.data.max(),
                                      keywordguide = {'keyword': 'IMAGETYP',
                                                      'flatvalue': 'Flat Field',
                                                      'biasvalue': 'Bias Frame'},
@@ -28,10 +106,10 @@ def calculate_shutter_correction_map(files, biases=None,
     biases = bytype.groups[bytype.groups.keys[keywordguide['keyword']]\
                            == keywordguide['biasvalue']]
 
-    if len(biases) > 0:
+    bias_images = [fits_ccddata_reader(os.path.join(files.location, f))
+                   for f in biases['file']]
+    if len(bias_images) > 0:
         ## Generate master bias to subtract from each flat
-        bias_images = [fits_ccddata_reader(os.path.join(files.location, f))
-                       for f in biases['file']]
         if len(bias_images) > 1:
             master_bias = combine(bias_images, combine='median')
         else:
@@ -48,55 +126,25 @@ def calculate_shutter_correction_map(files, biases=None,
         if len(flat_images) > 1:
             # Bias subtract flats
             if len(biases) > 0:
-                flat_images = [ccd.subtract_bias(im, master_bias) for im in flat_images]
-            sigma_clip = len(files) > 5  # perform sigma clipping if >5 files
-            combined_flat = ccd.combine(images, method='average',
+                flat_images = [subtract_bias(im, master_bias) for im in flat_images]
+            sigma_clip = len(flat_images) > 5  # perform sigma clipping if >5 files
+            combined_flat = combine(flat_images, method='average',
                                         sigma_clip=sigma_clip,
                                         sigma_clip_low_thresh=5,
                                         sigma_clip_high_thresh=5)
             combined_flats.append(combined_flat)
-            assert combined_flat.shape == flats[0].shape
+            assert combined_flat.shape == combined_flats[0].shape
         else:
             combined_flats.append(flat_images[0])
 
-    def nflat(flat, delta=25):
-        nl, nc = flat.data.shape
-        normalization_factor = np.median(flat.data[nl-delta:nl+delta,nc-delta:nc+delta])
-        nflat = flat.data / normalization_factor
-        return nflat
+    shutter_map = shutter_correction_algorithm(combined_flats, output=output)
+    return shutter_map
 
-    a = gain*np.sum([nflat(f) for f in combined_flats])
-    b = gain*np.sum([nflat(f)/float(f.header[exptimekey]) for f in combined_flats])
-    c = gain*np.sum([nflat(f)/float(f.header[exptimekey])**2 for f in combined_flats])
 
-    Dijcomb = ccd.combine(combined_flats, method='average')
-    Dij = gain*Dijcomb.data*len(combined_flats)
-
-    Eijm = [ccd.CCDData(data=f.data/float(f.header[exptimekey]), unit=f.unit)
-            for f in combined_flats]
-    Eijcomb = ccd.combine(Eijm, method='average')
-    Eij = gain*Eijcomb.data*len(combined_flats)
-
-    alpha_ij = 1.0 / (a*c - b**2) * (c*Dij - b*Eij)
-    delta_alpha = np.sqrt( c / (a*c - b**2) )
-    beta_ij = 1.0 / (a*c - b**2) * (a*Eij - b*Dij)
-    delta_beta = np.sqrt( a / (a*c - b**2) )
-
-    SF = beta_ij / alpha_ij  # SF = -t_SH * (1-SH_ij) in the paper terminology
-    delta_SF = np.sqrt( (alpha_ij**-1 * delta_beta)**2 + 
-                        (beta_ij * alpha_ij**-2 * delta_alpha)**2 )
-    SNR = np.mean(abs(SF)/delta_SF)
-    print('Typical pixel SNR of correction = {:.1f}'.format(SNR))
-    ShutterMap = ccd.CCDData(data=SF, uncertainty=delta_SF, unit=u.second,
-                     meta={'SNR': (SNR, 'Mean signal to noise per pixel')})
-
-    if output:
-        if os.path.exists(output):
-            os.remove(output)
-        ccd.fits_ccddata_writer(ShutterMap, output)
-
-    return ShutterMap
-
+def apply_shutter_correction(image, shutter_map, exptimekey='EXPTIME'):
+    exptime_map = shutter_map.add(image.header[exptimekey]*u.second)
+    result = image.divide(exptime_map)
+    return result
 
 
 if __name__ == '__main__':
