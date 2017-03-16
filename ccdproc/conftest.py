9db01af623cee977b7978715fffdaa9ed48fc58e
Michael Seifert <michaelseifert04@yahoo.de>
2017-03-16 11:31:44 +0100

    undo testing for deprecations [skip ci]

--- ccdproc/conftest.py
+++ ccdproc/conftest.py
@@ -18,7 +18,7 @@ TESTED_VERSIONS[packagename] = version
 
 # Uncomment the following line to treat all DeprecationWarnings as
 # exceptions
-enable_deprecations_as_exceptions()
+# enable_deprecations_as_exceptions()
 
 # Add astropy to test header information and remove unused packages.
 
