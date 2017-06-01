74c91354ad1d6cbce4c6808a51b5a418b03793e9
Matthew Craig <mattwcraig@gmail.com>
2017-06-01 11:36:22 -0500

    Add regression test for #457 handling of SIP in CTYPE

--- ccdproc/tests/setup_package.py
+++ ccdproc/tests/setup_package.py
@@ -2,4 +2,5 @@
 def get_package_data():
     return {
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc',
-                                            'data/a8280271.fits']}
+                                            'data/a8280271.fits',
+                                            'data/sip-wcs.fit']}
