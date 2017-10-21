5fc10d3edae9c224b74b4fe4d05c61ac0d25c0a1
Michael Seifert <michaelseifert04@yahoo.de>
2017-10-21 16:09:36 +0200

    Remove inappropriate comment from test_ccdmask file

--- ccdproc/combiner.py
+++ ccdproc/combiner.py
@@ -1,5 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-# This module implements the combiner class.
+
+"""This module implements the combiner class."""
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
--- ccdproc/core.py
+++ ccdproc/core.py
@@ -1,5 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-# This module implements the base CCDPROC functions
+
+"""This module implements the base CCDPROC functions"""
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
