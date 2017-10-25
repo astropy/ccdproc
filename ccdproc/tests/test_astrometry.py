d5be2366f0dfc712d9ded94bdd0083148c3de542
Steve Crawford <crawfordsm@gmail.com>
2017-10-25 16:35:51 +0200

    pep8 fixes on test_astrometry

--- ccdproc/tests/test_astrometry.py
+++ ccdproc/tests/test_astrometry.py
@@ -52,17 +52,18 @@ def test__calc_ratio():
 
     assert 5.0/13.0 == _calc_ratio(x, y, 0, 1, 2)
 
+
 # add test for zero
 def test__calc_ratio_zero():
     x = np.array([1, 4, 1])
     y = np.array([5, 5, 5])
 
     assert np.isnan(_calc_ratio(x, y, 0, 1, 2))
-    
 
 
 def test__get_index():
     assert _get_index(3, 3) == (1, 2, 0)
+
 
 # remove
 def test_distance_ratio():
@@ -75,9 +76,11 @@ def test_distance_ratio():
     result = [0.38461538, 2.6, 0.60633906, 1.64924225, 1.57648156, 0.63432394]
     np.testing.assert_almost_equal(r, result)
 
+
 # triangle_angle
 def test_triangle_angle():
-    assert triangle_angle(5,4,3) == np.pi/2.0
+    assert triangle_angle(5, 4, 3) == np.pi/2.0
+
 
 # calc_triangle
 def calc_triangle():
@@ -86,70 +89,64 @@ def calc_triangle():
 
     sides, angles, order = astrom.calc_triangle(x, y, 0, 1, 2)
 
-    sides_result = np.array([ 0.38461538, 1., 0.63432394])
+    sides_result = np.array([0.38461538, 1., 0.63432394])
     np.testing.assert_almost_equal(sides, sides_result)
-    angles_result = array([ 0.14981246, 2.74307021, 0.24870999])
+    angles_result = array([0.14981246, 2.74307021, 0.24870999])
     np.testing.assert_almost_equal(angles, angles_result)
     assert order == np.array(0, 2, 1)
 
-# match_by_triangle
 
 # match_by_fit
 def test_match_by_fit():
-    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1= 0.2)
-    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1= 0.1)
+    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1=0.2)
+    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1=0.1)
 
     x = np.arange(10)
     y = np.arange(10)
 
-    d = y_init(x,y)
-    r = x_init(x,y)
+    d = y_init(x, y)
+    r = x_init(x, y)
 
-    rr, dd = match_by_fit(x,y,r,d,[0,1,2], [0,1,2],
-                                  tolerance=0.1*u.degree)
+    rr, dd = match_by_fit(x, y, r, d, [0, 1, 2], [0, 1, 2],
+                          tolerance=0.1*u.degree)
 
-
-    assert np.all(rr==np.arange(10))
-    assert np.all(dd==np.arange(10))
+    assert np.all(rr == np.arange(10))
+    assert np.all(dd == np.arange(10))
 
 
 # match_by_fit
 def test_match_by_fit_quantity():
-    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1= 0.2)
-    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1= 0.1)
+    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1=0.2)
+    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1=0.1)
 
     x = np.arange(10)
     y = np.arange(10)
 
-    d = y_init(x,y)
-    r = x_init(x,y)
+    d = y_init(x, y)
+    r = x_init(x, y)
 
-    rr, dd = match_by_fit(x,y,r*u.deg,d*u.deg,[0,1,2], [0,1,2],
-                                  tolerance=0.1*u.degree)
+    rr, dd = match_by_fit(x, y, r*u.deg, d*u.deg, [0, 1, 2], [0, 1, 2],
+                          tolerance=0.1*u.degree)
 
-
-    assert np.all(rr==np.arange(10))
-    assert np.all(dd==np.arange(10))
-
+    assert np.all(rr == np.arange(10))
+    assert np.all(dd == np.arange(10))
 
 
 # create_wcs_from_fit
 def test_create_wcs_from_fit():
-    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1= 0.2)
-    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1= 0.1)
+    x_init = mod.models.Polynomial2D(1, c0_0=10, c1_0=0.1, c0_1=0.2)
+    y_init = mod.models.Polynomial2D(1, c0_0=5, c1_0=0.2, c0_1=0.1)
 
     x = np.arange(10)
     y = np.arange(10)
-  
-    d = y_init(x,y)
-    r = x_init(x,y)
+
+    d = y_init(x, y)
+    r = x_init(x, y)
 
     idp = np.arange(10)
-  
-    wcs = create_wcs_from_fit(x, y, r, d, idp, idp, xref=0, yref=0) 
-    rr, dd = wcs.all_pix2world(x,y,1)
-    # having trouble testing almost_equal
-    #assert np.test.assert_almost_equal(rr, r)
-    #assert np.testing.assert_almost_equal(dd, d)
-    assert abs(rr-r).mean() < 1e-2 
-    assert abs(dd-d).mean() < 1e-2 
+
+    wcs = create_wcs_from_fit(x, y, r, d, idp, idp, xref=0, yref=0)
+    rr, dd = wcs.all_pix2world(x, y, 1)
+
+    assert abs(rr-r).mean() < 1e-2
+    assert abs(dd-d).mean() < 1e-2
