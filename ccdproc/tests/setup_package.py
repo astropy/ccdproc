321c51ca9b70636e6a6f3632d9d0d2992298b1a5
Matthew Craig <mattwcraig@gmail.com>
2019-08-03 18:02:55 -0500

    Add small sample multi-extension FITS files and code to generate them

--- ccdproc/tests/setup_package.py
+++ ccdproc/tests/setup_package.py
@@ -4,4 +4,6 @@ def get_package_data():
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc',
                                             'data/a8280271.fits',
                                             'data/sip-wcs.fit',
-                                            'data/expected_ifc_file_properties.csv']}
+                                            'data/expected_ifc_file_properties.csv',
+                                            'data/science-mef.fits',
+                                            'data/flat-mef.fits']}
